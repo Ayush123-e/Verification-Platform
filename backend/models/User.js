b8e8553aaf912c390e12fb6e6a576edf0b376fdd
Ayush Kumar <ayushnbt94@gmail.com>
2026-05-21 08:40:06 +0530

    Fix signup issue: Remove duplicate password hashing in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -27,18 +27,6 @@ const UserSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-UserSchema.pre('save', async function() {
-  if (!this.isModified('password')) {
-    return;
-  }
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  } catch (error) {
-    throw error;
-  }
-});
-
 // Compare password method
 UserSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
